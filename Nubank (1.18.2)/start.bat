87ac90b45f3ff620dc70fc07d6c9909b189a4e6e
be愛tri√.ᴘʏ $ <84352260+beahtrwzll@users.noreply.github.com>
2022-04-10 01:51:28 -0300

    Upload Nubank Server

---
+++ Nubank (1.18.2)/start.bat
@@ -0,0 +1,4 @@
+:: Run file for Windows
+
+java -Xmx4G -Xms4G -jar insert-spigot-file-here.jar nogui
+pause
---
+++ Nubank (1.18.2)/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Run file for Linux
+
+java -Xmx4G -Xms4G -jar insert-spigot-file-here.jar nogui
