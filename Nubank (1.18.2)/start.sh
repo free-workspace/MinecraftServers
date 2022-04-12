0c1e1c8ed4deabe552c002829d221cbfa38cb733
be愛tri√.ᴘʏ $ <guilherme.access@protonmail.com>
2022-04-12 08:34:46 -0300

    Update start.sh

--- Nubank (1.18.2)/start.sh
+++ Nubank (1.18.2)/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Run file for Linux
 
-java -Xmx4G -Xms4G -jar insert-spigot-file-here.jar nogui
+java -Xmx<insert-the-amount-of-RAM>G -Xms<insert-the-amount-of-RAM>G -jar <insert-spigot-file-name-here>.jar nogui
