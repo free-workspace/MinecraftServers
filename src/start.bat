c2afc74e1670d02f9f1421b43c05401280b82a54
be愛tri√.ᴘʏ $ <84352260+beahtrwzll@users.noreply.github.com>
2022-04-23 12:19:05 -0300

    Update Fundão's Server

--- src/start.bat
+++
@@ -1,4 +0,0 @@
-:: Run file for Windows
-
-java -Xmx<insert-the-amount-of-RAM>G -Xms<insert-the-amount-of-RAM>G -jar <insert-spigot-file-name-here>.jar nogui
-pause
--- src/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Run file for Linux
-
-java -Xmx<insert-the-amount-of-RAM>G -Xms<insert-the-amount-of-RAM>G -jar <insert-spigot-file-name-here>.jar nogui
